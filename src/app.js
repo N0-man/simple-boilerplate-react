7348c94087e7032937d76981b28023cdb5a2a7e2
_n0man <nouman.memon@icloud.com>
2018-08-21 10:43:18 +0530

    _n0man | fixed boilerplate for yarn install

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<h4>Welcome to _n0man's boilerplate react-app</h4>, document.getElementById('app'));
